f54a440e9a6448c3830bfd9e08112c3abc524b4b
Timothy Farkas <tim@Timothys-MacBook-Pro.local>
2022-02-26 16:26:48 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of tplant trait relatationships to populations dynamics in the Sevilleta Wildlife Refuge, Socorro County, New Mexico',
+    author='Tim Farkas',
+    license='MIT',
+)
